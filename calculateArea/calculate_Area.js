4ead5b258826ad0d414b74011df18d013a359b88
ThembaMthimunye <218634567@tut4life.ac.za>
2026-03-13 18:30:55 -0400

    pactice

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -1,12 +1,25 @@
 let length;
 let width;
 
-function calculateArea()
-{
-    length=parseFloat(document.getElementById("length").value)
-    width=parseFloat(document.getElementById("width").value)
-    let area=length*width
-    console.log(area)
-    document.getElementById("result").innerHTML=`The are of the square is :${area}`
-}
+let grocery_1;
+let grocery_2;
+let grocery_3;
 
+
+// function calculateArea()
+// {
+//     length=parseFloat(document.getElementById("length").value)
+//     width=parseFloat(document.getElementById("width").value)
+//     let area=length*width
+//     console.log(area)
+//     document.getElementById("result").innerHTML=`The are of the square is :${area}`
+// }
+
+function calculateTotal(){
+    grocery_1=parseFloat(document.getElementById("grocery_1").value)
+    grocery_2=parseFloat(document.getElementById("grocery_2").value)
+    grocery_3=parseFloat(document.getElementById("grocery_3").value)
+
+    let total =grocery_1+grocery_2+grocery_3;
+    document.getElementById("grocery_result").innerHTML=`The total amaount for the grocery is ${total}`
+}
