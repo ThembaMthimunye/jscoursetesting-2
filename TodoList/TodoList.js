74780f05118368286cf0e8e7a41511bd5798853e
ThembaMthimunye <218634567@tut4life.ac.za>
2026-03-15 10:40:24 -0400

    Themba

---
+++ TodoList/TodoList.js
@@ -0,0 +1,41 @@
+const taskInput = document.getElementById("taskInput");
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskList = document.getElementById("taskList");
+const clearCompletedBtn = document.getElementById("clearCompletedBtn");
+
+let tasks = [];
+
+function addTask(){
+    const taskText=taskInput.ariaValueMax.trim()
+    if(taskText!=="")
+    {
+        taskInput.push({text:taskText})
+        taskInput.value="";
+        displayTasks()
+    }
+}
+
+function displayTasks(){
+    taskList.innerHTML=""
+    tasks.forEach((task,index)=>{
+    const li=document.createElementById('li')
+    li.innerHTML=`{<input type="checkbox" id="task-${index}" ${task.completed ? "checked" : ""}>
+                    <label for ="task-${index} >${task.text}"></label>} `
+    taskList.appendChild(li)
+    }
+)
+}
+
+function toggleTask(index) {
+    tasks[index].completed = !tasks[index].completed;
+    displayTasks();
+}
+
+function clearCompletedTasks()
+{
+    tasks=tasks.filter(task=>!task.completed)
+    displayTasks();
+}
+
+addTaskBtn.addEventLister("click",addTask);
+clearCompletedBtn.addEventLister("click",clearCompletedTasks)
